1ca3d112659eec8fdacbe8f5cad5cf7bb5b079fb
Antoine Barthelemy <antoine@linux.com>
2022-03-09 16:02:48 +0100

    feat(tests): add basic tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,23 @@
 import Test.Hspec
 import Lib
+import Data
 
 main :: IO ()
 main = hspec $ do
-  describe "How to write a test" $ do
-    it "Should be able to run tests" $ do
-      someString `shouldBe` "someString"
-      someString `shouldBe` "anotherString"
+  describe "formatGrid" $ do
+    it "Should concatenate every line with a newline" $ do
+      (formatGrid ["abc","def","ghi"]) `shouldBe` "abc\ndef\nghi\n"
+
+  describe "findWords" $ do
+    it "Should find words that exists on the Grid" $ do
+      findWord grid "HASKELL" `shouldBe` Just "HASKELL"
+      findWord grid "PERL" `shouldBe` Just "PERL"
+      findWord grid "RUBY" `shouldBe` Just "RUBY"
+    it "Should not find words on the Grid" $ do
+      findWord grid "HAMSTER" `shouldBe` Nothing
+
+  describe "findWords" $ do
+    it "Should find all the words that exists on the Grid" $ do
+      findWords grid languages `shouldBe` languages
+    it "Should not find words that do not exists on the Grid" $ do
+      findWords grid ["GERMAN", "FRENCH", "TYPESCRIPT"] `shouldBe` []
